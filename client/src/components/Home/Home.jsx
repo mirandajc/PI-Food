cd5a9ec46ac87b42688a7963a5b5cfe68bb15fbb
mirandajc <74868639+mirandajc@users.noreply.github.com>
2022-05-10 23:48:47 -0300

    pagination ok

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,9 +1,9 @@
-import React, {useEffect} from "react"
+import React, {useEffect, useState} from "react"
 import NavBar from '../Navbar/Navbar'
 import RecipeCard from "../Recipe-Card/RecipeCard"
 import { useDispatch, useSelector } from "react-redux";
 import { allRecipes, allTypes } from "../../Redux/actions/actions.js";
-//import Pagination from "../Pagination/Pagination";
+import Pagination from "../Pagination/Pagination";
 //import { Link } from "react-router-dom";
 
 
@@ -19,10 +19,9 @@ function Home(){
     
 
     //------Paginacion
-    // const [pag, setPag] = useState(1);
-    // const [recipesPag , setRecipesPag] = useState(9);
-
-    // const max = recipes.length /recipesPag;
+    const [pag, setPag] = useState(1);
+    const [recipesPag , setRecipesPag] = useState(9);
+    const max = Math.ceil(recipes.length /recipesPag);
 
 
     useEffect(()=>{
@@ -36,9 +35,12 @@ function Home(){
         <div>
                 <h2>Bienvenidos</h2>
                 <NavBar/>
-                {/* <Pagination pagina={pag} setPag={setPag} maximo={max} /> */}
+                <Pagination pag={pag} setPag={setPag} max={max} />
                 <div>
-                {recipes?.map(recipe => {
+                {recipes.slice(
+                    (pag-1)* recipesPag, 
+                    (pag-1)* recipesPag+ recipesPag
+                )?.map(recipe => {
                     return (
                         <RecipeCard image={recipe.image} name={recipe.name} id={recipe.id} type={recipe.type?.map(diet =>  <p>{diet.name}</p> )} key={Math.random().toString(36).substr(2, 9)} ></RecipeCard>
                         )
--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 
 export default function Pagination({pag, setPag, max}) {
+    console.log(max)
     const [input,setInput] = useState(1);
-    function nextPage() {
+    const nextPage = () => {
         setInput(input + 1);
         setPag( pag + 1)
     }
-    function prevPage() {
-        setInput(input -1);
-        setPag( pag - 1)
+    const prevPage = () => {
+        setInput    (input -1);
+        setPag  (pag - 1)
     }
 
     return (
         <div>
-            <button onClick={prevPage}>prev</button>
+            <button onClick={prevPage} >prev</button>
             <input name="pag" autoComplete="off" value={input} />
-            <p>{max}</p>
-            <button onClick={nextPage }>next</button>
+            <p> de {max}</p>
+            <button onClick={nextPage} >next</button>
         </div>
     )
 }
